faaf6bf01a3d281a90e99bd448e7d8d548a3c7d7
Sumra Khalid <sumrakhalidev@gmail.com>
2022-06-15 08:29:26 -0400

    changed content and spacing

--- src/components/Form/Congratulations.js
+++ src/components/Form/Congratulations.js
@@ -37,7 +37,7 @@ class Congratulations extends Component{
                     <div className='text-center mb-5'>
                         <h3 className='text-secondary '>Next Steps and What to Expect</h3>
                     </div>
-                    <InlineWidget styles={{height: '1000px'}} url={configData.CALENDLY_CONTRACT_LINK} />
+                    <InlineWidget styles={{height: '800px'}} url={configData.CALENDLY_CONTRACT_LINK} />
                     <div class="m-auto text-left text-gray">
                         <Card className="border-0 border-bottom my-3 p-2">
                             <div className="d-flex m-2 ms-0 justify-content-start gap-3">
@@ -49,7 +49,7 @@ class Congratulations extends Component{
                                         Sign Contract
                                     </h4>
                                     <p className="mb-0 text-secondary">
-                                    After the inspection, we will create a contract for the purchase of your home for your approval. The contract will detail the property details, pricing, closing dates, and legal responsibilities of each party.
+                                        Please schedule a time on the Calendar above so we can go over the contract to purchase your home. The contract will detail the property details, pricing, closing dates, and legal responsibilities of each party.
                                     </p>
                                     
                                 </div>
@@ -65,7 +65,7 @@ class Congratulations extends Component{
                                         Open Escrow
                                     </h4>
                                     <p className="mb-0 text-secondary">
-                                        Once the contract is executed by both parties, we forward the contract and earnest money deposit to one of our preferred title companies. The title company will do a title examination, where they search the title history of the property, including previous deeds, liens, taxes due, judgments, easements, and anything else that might affect title to the property
+                                        Once the contract is executed by both parties, we forward the contract and earnest money deposit to one of our preferred title companies. The title company will do a title examination, where they search the title history of the property, including previous deeds, liens, taxes due, judgments, easements, and anything else that might affect the title to the property
                                     </p>
                                     
                                 </div>
@@ -81,7 +81,7 @@ class Congratulations extends Component{
                                         Closing
                                     </h4>
                                     <p className="mb-0 text-secondary">
-                                        Once the title company has completed the title examination, and cleared any issues that may have been discovered during that research, we will schedule the closing where all parties sign the documents necessary to transfer ownership of the property from you to Sell My House Fast SA TX. We send a mobile notary to you to sign all the closing documents.
+                                        Once the title company has completed the title examination, and title has been cleared, we will schedule the closing where all parties sign the documents necessary to transfer ownership of the property from you to Sell My House Fast SA TX. 
                                     </p>
                                     
                                 </div>
@@ -97,7 +97,7 @@ class Congratulations extends Component{
                                         Funding
                                     </h4>
                                     <p className="mb-0 text-secondary">
-                                        After all parties have signed the closing documents, the title company will review and verify the signatures, and then wire your cash to your bank account within 24 hours of signing closing documents.
+                                        After all, parties have signed the closing documents, the title company will review and verify the signatures and then wire your cash to your bank account within 24 hours of signing the closing documents.
                                     </p>
                                     
                                 </div>
